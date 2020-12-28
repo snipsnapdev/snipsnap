e5480840ff3b040096852df457cb1b979df53920
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-12-28 14:03:42 +0300

    feat: add antd with custom theming and css modules with sass

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+// import '~antd/dist/antd.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
