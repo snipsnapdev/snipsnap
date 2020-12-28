7070de4cd4d12fdd3e2950664ee8848af7d520d7
Anastasia Videneeva <nastya.videneeva@gmail.com>
2020-12-28 10:21:08 +0300

    feat: add monaco editor

---
+++ next.config.js
@@ -0,0 +1,36 @@
+const withCSS = require('@zeit/next-css');
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+
+module.exports = withCSS({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+        },
+      },
+    });
+
+    config.plugins.push(
+      new MonacoWebpackPlugin({
+        // Add languages as needed...
+        languages: [
+          'javascript',
+          'typescript',
+          'json',
+          'markdown',
+          'css',
+          'scss',
+          'html',
+          'python',
+          'yaml',
+        ],
+        filename: 'static/[name].worker.js',
+      })
+    );
+
+    return config;
+  },
+});
--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dynamic from 'next/dynamic';
 
-const Editor = () => <div>TODO: editor</div>;
+const MonacoEditor = dynamic(import('react-monaco-editor'), { ssr: false });
+
+const Editor = () => {
+  const [code, setCode] = useState('');
+  // etc
+  return (
+    <div>
+      <MonacoEditor
+        editorDidMount={() => {
+          window.MonacoEnvironment.getWorkerUrl = (_moduleId, label) => {
+            if (label === 'json') return '_next/static/json.worker.js';
+            if (label === 'css') return '_next/static/css.worker.js';
+            if (label === 'html') return '_next/static/html.worker.js';
+            if (label === 'typescript' || label === 'javascript')
+              return '_next/static/ts.worker.js';
+            return '_next/static/editor.worker.js';
+          };
+        }}
+        language="css"
+        width="800"
+        height="600"
+        theme="vs-dark"
+        value={code}
+        options={{
+          minimap: {
+            enabled: false,
+          },
+        }}
+        onChange={setCode}
+      />
+    </div>
+  );
+};
 
 export default Editor;
