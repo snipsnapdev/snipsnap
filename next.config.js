d6f0be533d5109bfc4bc91143293ba000e903416
vannnyle@gmail.com <vannnyle@gmail.com>
2020-12-30 14:02:06 +0200

    feat(components): add hover state for Button

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-    prependData: '@import "./src/styles/variables.scss";',
+    prependData: '@import "./src/styles/mixins.scss", "./src/styles/variables.scss";',
   },
   webpack(config) {
     config.module.rules.push({
