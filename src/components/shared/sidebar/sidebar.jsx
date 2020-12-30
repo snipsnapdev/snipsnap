627c8739745096e1679340f84153cd6dca3d1182
AndrewKirpanev <spacewebdeveloper@gmail.com>
2020-12-30 21:47:28 +1000

    feat: add block DropdownMenu

--- src/components/shared/sidebar/sidebar.jsx
+++ src/components/shared/sidebar/sidebar.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
 import Link from 'next/link';
+import { signOut } from 'next-auth/client';
 import Menu from './menu/menu';
 
 import styles from './sidebar.module.scss';
+import DropdownMenu from '../dropdown-menu/dropdown-menu';
 
 const cx = classNames.bind(styles);
 
 const Sidebar = ({ userName }) => {
   const avatar = userName.slice(0, 1);
+  const [isDropDownUserOpen, setIsDropDownUserOpen] = useState(false);
+
+  const handleUserClick = () => {
+    setIsDropDownUserOpen(!isDropDownUserOpen);
+  };
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('user-wrapper')}>
+      <div className={cx('user-wrapper')} onClick={handleUserClick}>
         <div className={cx('user-info')}>
           <div className={cx('avatar-wrapper')}>
             <div className={cx('avatar')}>{avatar}</div>
           </div>
           <span>{userName}</span>
+
+          <DropdownMenu className={cx('user-info-dropdown')} isOpen={isDropDownUserOpen}>
+            <button onClick={signOut}>Log out</button>
+          </DropdownMenu>
         </div>
         <Link href="#">
           <a className={cx('button')}>Create Template</a>
