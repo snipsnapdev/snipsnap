9a417e32ecc4c809bf933f777c61f05f04c3d010
vannnyle@gmail.com <vannnyle@gmail.com>
2020-12-30 13:39:31 +0200

    feat(components): add shared component Button

--- src/components/shared/sidebar/sidebar.jsx
+++ src/components/shared/sidebar/sidebar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
-import Link from 'next/link';
 import Menu from './menu/menu';
 
 import styles from './sidebar.module.scss';
+import Button from '../button/button';
 
 const cx = classNames.bind(styles);
 
-const Sidebar = ({ userName }) => {
+const Sidebar = ({ userName, buttonText }) => {
   const avatar = userName.slice(0, 1);
   return (
     <div className={cx('wrapper')}>
@@ -20,16 +20,14 @@ const Sidebar = ({ userName }) => {
           </div>
           <span>{userName}</span>
         </div>
-        <Link href="#">
-          <a className={cx('button')}>Create Template</a>
-        </Link>
+        <Button theme="primary" to="#">
+          {buttonText}
+        </Button>
       </div>
       <Menu />
       <div className={cx('templates')}>
         <h3>Templates groups</h3>
-        <Link href="#">
-          <a />
-        </Link>
+        <Button type="plus" to="#" />
       </div>
     </div>
   );
@@ -39,6 +37,7 @@ Sidebar.propTypes = {};
 
 Sidebar.defaultProps = {
   userName: 'Alex Barashkov',
+  buttonText: 'Create template',
 };
 
 export default Sidebar;
