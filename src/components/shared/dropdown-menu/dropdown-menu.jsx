df07c83fc1d22bc39d46fb09a28a4b4619f2a09f
AndrewKirpanev <spacewebdeveloper@gmail.com>
2020-12-31 15:24:58 +1000

    refactor(components): optimize close handler for dropdown menu

--- src/components/shared/dropdown-menu/dropdown-menu.jsx
+++ src/components/shared/dropdown-menu/dropdown-menu.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 
@@ -7,18 +7,17 @@ import styles from './dropdown-menu.module.scss';
 const cx = classNames.bind(styles);
 
 const DropdownMenu = ({ className: additionalClassName, children }) => {
+  const wrapperRef = useRef();
+
   const handleClose = ({ target }) => {
-    const wrapper = document.querySelectorAll(`.${styles.wrapper}`);
+    const { current: dropdown } = wrapperRef;
+    const parentDropdown = dropdown.parentNode;
 
-    wrapper.forEach((dropdown) => {
-      const parentDropdown = dropdown.parentNode;
-
-      if (!parentDropdown.contains(target) && !dropdown.contains(target)) {
-        dropdown.classList.remove(`${styles.active}`);
-      } else {
-        dropdown.classList.add(`${styles.active}`);
-      }
-    });
+    if (!parentDropdown.contains(target) && !dropdown.contains(target)) {
+      dropdown.classList.remove(`${styles.active}`);
+    } else {
+      dropdown.classList.add(`${styles.active}`);
+    }
   };
 
   useEffect(() => {
@@ -28,7 +27,11 @@ const DropdownMenu = ({ className: additionalClassName, children }) => {
     };
   }, []);
 
-  return <div className={cx('wrapper', additionalClassName)}>{children}</div>;
+  return (
+    <div className={cx('wrapper', additionalClassName)} ref={wrapperRef}>
+      {children}
+    </div>
+  );
 };
 
 DropdownMenu.propTypes = {
