b2fa9bc6d2c07b71e5013818cbad0f69417537cf
Alex Barashkov <lnikell@gmail.com>
2021-01-28 21:42:22 +0100

    feat: change auth logic

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.scss';
 import { Provider } from 'next-auth/client';
 
 function Snipsnap({ Component, pageProps }) {
+  console.log(pageProps);
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
