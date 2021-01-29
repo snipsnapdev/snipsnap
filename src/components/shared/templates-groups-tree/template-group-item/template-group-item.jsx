f7e24f66e78e1c75d91101b8731d9ce1922ee385
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-01-29 13:46:37 +0300

    feat: delete group (wip)

--- src/components/shared/templates-groups-tree/template-group-item/template-group-item.jsx
+++ src/components/shared/templates-groups-tree/template-group-item/template-group-item.jsx
@@ -1,6 +1,9 @@
 import classNames from 'classnames/bind';
+import { useSession } from 'next-auth/client';
 import { useState } from 'react';
+import { mutate } from 'swr';
 
+import { gql, gqlClient } from 'api/graphql';
 import Dropdown from 'components/shared/dropdown';
 import ArrowSvg from 'icons/arrow-down.inline.svg';
 import DotsSvg from 'icons/dots-menu.inline.svg';
@@ -12,14 +15,48 @@ import styles from './template-group-item.module.scss';
 
 const cx = classNames.bind(styles);
 
+const query = gql`
+  mutation deleteTemplateGroup($id: uuid!) {
+    delete_templates_groups_by_pk(id: $id) {
+      name
+      user_id
+      id
+    }
+  }
+`;
+
 const TemplateGroupItem = ({ name, groupId, templates }) => {
   const [isExpanded, setIsExpanded] = useState(false);
-  const groupMenu = (
-    <>
-      <a href="#">Edit</a>
-      <a href="#">Delete</a>
-    </>
-  );
+
+  const [{ token }] = useSession();
+  const [loading, setLoading] = useState(false);
+
+  const onDelete = async (groupId) => {
+    try {
+      setLoading(true);
+      await gqlClient(token).request(query, { id: groupId });
+      setLoading(false);
+      mutate('getOwnedTemplatesGroups');
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+  };
+
+  const groupMenu = (groupId) => (
+      <>
+        <a href="#">Edit</a>
+        {/* <a href="#">Delete</a> */}
+        <div onClick={() => onDelete(groupId)}>Delete</div>
+      </>
+    );
+
+  // const groupMenu = (
+  //   <>
+  //     <a href="#">Edit</a>
+  //     <a href="#">Delete</a>
+  //   </>
+  // );
 
   const handleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -35,7 +72,8 @@ const TemplateGroupItem = ({ name, groupId, templates }) => {
         {templates.length > 0 && <ArrowSvg className={cx('arrow')} />}
         <div className={cx('options')}>
           <Dropdown
-            menu={groupMenu}
+            menu={groupMenu(groupId)}
+            // menu={groupMenu}
             className={cx('options-inner')}
             position="top-right"
             stopPropagation
