f4197cb459b336481bd6e941fe1d77e82e0b6bbc
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-03-26 16:46:50 +0300

    refactor: remove editor and file-browser pages

--- src/pages/editor.js
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const Editor = dynamic(import('components/shared/editor'), { ssr: false });
-
-const EditorPage = () => <Editor />;
-
-export default EditorPage;
--- src/pages/file-browser.js
+++
@@ -1,9 +0,0 @@
-import FileBrowser from 'components/shared/template-form/files/file-browser';
-
-const FileBrowserPage = () => (
-  <div style={{ marginTop: 200, marginLeft: 200 }}>
-    <FileBrowser />
-  </div>
-);
-
-export default FileBrowserPage;
