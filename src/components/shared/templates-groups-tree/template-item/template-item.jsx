f2f3039a757a083932f84b5df2a103040a99396d
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-04-02 20:09:34 +0300

    feat: add dropdown menu for templates

--- src/components/shared/templates-groups-tree/template-item/template-item.jsx
+++ src/components/shared/templates-groups-tree/template-item/template-item.jsx
@@ -3,7 +3,10 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 import DeleteTemplateModal from 'components/shared/delete-template-modal';
+import Dropdown from 'components/shared/dropdown';
+import ShareModal from 'components/shared/share-modal';
 import CloseSvg from 'icons/close.inline.svg';
+import DotsSvg from 'icons/dots-menu.inline.svg';
 import TemplateSvg from 'icons/template.inline.svg';
 
 import styles from './template-item.module.scss';
@@ -12,20 +15,47 @@ const cx = classNames.bind(styles);
 
 const TemplateItem = ({ name, templateId, nested }) => {
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [isShareModalOpen, setIsShareModalOpen] = useState(false);
+
+  const templateMenu = (
+    <>
+      <div onClick={() => setIsShareModalOpen(true)}>Share</div>
+      <div onClick={() => setIsDeleteModalOpen(true)}>Delete</div>
+    </>
+  );
+
   return (
     <Link href={`/template/${templateId}`}>
       <div className={cx('wrapper', { nested })}>
         <TemplateSvg className={cx('icon')} />
         <span className={cx('name')}>{name}</span>
-        <div className={cx('delete-icon')} onClick={() => setIsDeleteModalOpen(true)}>
-          <CloseSvg />
+        <div className={cx('options')}>
+          <Dropdown
+            menu={templateMenu}
+            className={cx('options-inner')}
+            position="top-right"
+            stopPropagation
+          >
+            <DotsSvg className={cx('options-icon')} />
+          </Dropdown>
         </div>
+        {/* <div className={cx('delete-icon')} onClick={() => setIsDeleteModalOpen(true)}>
+          <CloseSvg />
+        </div> */}
         {isDeleteModalOpen && (
           <DeleteTemplateModal
             id={templateId}
             name={name}
             isOpen={isDeleteModalOpen}
             onClose={() => setIsDeleteModalOpen(false)}
+          />
+        )}
+        {isShareModalOpen && (
+          <ShareModal
+            id={templateId}
+            type="template"
+            isOpen={isShareModalOpen}
+            onClose={() => setIsShareModalOpen(false)}
           />
         )}
       </div>
