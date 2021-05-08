336fa0242b8a615c7b3de0d74f62dabd4b3defde
Alex Barashkov <lnikell@gmail.com>
2021-05-08 13:56:06 +0200

    feat: add Sentry

--- src/pages/api/auth/token.js
+++ src/pages/api/auth/token.js
@@ -1,10 +1,11 @@
+import { withSentry } from '@sentry/nextjs';
 import { getSession } from 'next-auth/client';
 
 import jwt from 'utils/jwt';
 
 const MAX_AGE = process.env.JWT_API_MAX_AGE;
 
-export default async (req, res) => {
+const refreshToken = async (req, res) => {
   const session = await getSession({ req });
 
   if (!session) {
@@ -13,3 +14,5 @@ export default async (req, res) => {
   const token = jwt.encode(session.user.id, MAX_AGE);
   res.json({ token });
 };
+
+export default withSentry(refreshToken);
