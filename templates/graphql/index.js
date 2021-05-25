2e4cc682e9f73dcd239060e0295d350484863277
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-05-25 11:13:04 +0300

    refactor: move graphql server type definitions to a separate file

--- templates/graphql/index.js
+++ templates/graphql/index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const { ApolloServer, gql } = require("apollo-server");
 const { GraphQLClient } = require("graphql-request");
 
+const { typeDefs } = require("./types");
+
 const { encode } = require("./utils/jwt");
 const {
   validatePrompts,
@@ -65,91 +67,6 @@ const getUserByEmail = async (email) => {
 
   return data?.users?.[0]?.user_id;
 };
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-
-  input InsertTemplateInput {
-    name: String!
-    prompts: String
-    files: String!
-    template_group_id: String
-  }
-
-  input UpdateTemplateInput {
-    id: String!
-    name: String
-    prompts: String
-    files: String
-    template_group_id: String
-  }
-
-  type Template {
-    id: String
-    name: String
-    prompts: String
-    files: String
-    template_group_id: String
-    owner_id: String
-    created_at: String
-    updated_at: String
-  }
-
-  input ShareTemplateInput {
-    template_id: String!
-    share_to_user_email: String!
-    share_by_user_id: String
-  }
-
-  type SharedTemplate {
-    id: String
-    template_id: String
-    shared_by_user_id: String
-    shared_to_user_id: String
-    created_at: String
-    updated_at: String
-  }
-
-  input ShareTemplateGroupInput {
-    template_group_id: String!
-    share_to_user_email: String!
-  }
-
-  type SharedTemplateGroup {
-    id: String
-    template_group_id: String
-    shared_by_user_id: String
-    shared_to_user_id: String
-    created_at: String
-    updated_at: String
-  }
-
-  input RefreshApiTokenInput {
-    user_id: String!
-  }
-
-  type RefreshApiToken {
-    api_key: String
-  }
-
-  type Mutation {
-    insert_template(object: InsertTemplateInput): Template
-    update_template(object: UpdateTemplateInput): Template
-    share_template(object: ShareTemplateInput): SharedTemplate
-    unshare_template(object: ShareTemplateInput): SharedTemplate
-    unshare_template_from_current_user(
-      object: ShareTemplateInput
-    ): SharedTemplate
-    share_template_group(object: ShareTemplateGroupInput): SharedTemplateGroup
-    unshare_template_group(object: ShareTemplateGroupInput): SharedTemplateGroup
-    unshare_template_group_from_current_user(
-      object: ShareTemplateGroupInput
-    ): SharedTemplateGroup
-    refresh_api_token(object: RefreshApiTokenInput): RefreshApiToken
-  }
-`;
 
 const resolvers = {
   Mutation: {
