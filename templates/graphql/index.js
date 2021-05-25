4f889d563406da73af42b9922bf8eb5aac4f4dd3
Anastasia Videneeva <nastya.videneeva@gmail.com>
2021-05-25 11:09:20 +0300

    refactor: move jwt encoding and validaiton helpers to separate files

--- templates/graphql/index.js
+++ templates/graphql/index.js
@@ -3,30 +3,13 @@ require("dotenv").config();
 const { ApolloServer, gql } = require("apollo-server");
 const { GraphQLClient } = require("graphql-request");
 
-const jwt = require("jsonwebtoken");
-
-const { JWT_SECRET } = process.env;
-
-const ENCRYPTION_ALGORITHM = "HS512";
-
-const encode = (userId, expires, role = "api-user") => {
-  const tokenContent = {
-    "https://hasura.io/jwt/claims": {
-      "x-hasura-allowed-roles": [role],
-      "x-hasura-default-role": role,
-      "x-hasura-role": role,
-      "x-hasura-user-id": userId,
-    },
-  };
-  const encodedToken = jwt.sign(tokenContent, JWT_SECRET, {
-    algorithm: ENCRYPTION_ALGORITHM,
-    expiresIn: `${expires}s`,
-  });
-  return encodedToken;
-};
-
-const MAX_FILE_SIZE = 5000; /// 5KB
-const MAX_FILES_AMOUNT = 20;
+const { encode } = require("./utils/jwt");
+const {
+  validatePrompts,
+  validateSizesOfFiles,
+  validateAmountOfFiles,
+  validateFiles,
+} = require("./utils/validation");
 
 const gqlClient = new GraphQLClient(process.env.HASURA_GRAPHQL_URL, {
   headers: {
@@ -64,62 +47,6 @@ const shareTemplateQuery = gql`
     }
   }
 `;
-
-const validatePrompts = (prompts) => {
-  if (!Array.isArray(prompts)) return false;
-
-  const variableNames = prompts.map(({ variableName }) => variableName);
-
-  return (
-    // Checks if prompts have multiple variableName with the same value or not
-    new Set(variableNames).size === prompts.length &&
-    // Checks if every prompt has both message and variableName
-    prompts.every(({ message, variableName }) => message && variableName)
-  );
-};
-
-const validateSizesOfFiles = (files) => {
-  const callback = ({ type, data }) => {
-    if (type === "file") {
-      const fileSize = Buffer.byteLength(data.content, "utf8");
-      if (fileSize > MAX_FILE_SIZE) {
-        console.log("file", data.name, "is too large: ", fileSize, "bites");
-      }
-      return fileSize <= MAX_FILE_SIZE;
-    } else {
-      return data.files.every(callback);
-    }
-  };
-
-  return files.every(callback);
-};
-
-// Ignores folders, counts only files
-const validateAmountOfFiles = (files) => {
-  let amountOfFiles = 0;
-
-  const callback = ({ type, data }) => {
-    if (type === "file") {
-      amountOfFiles++;
-    } else {
-      data.files.forEach(callback);
-    }
-  };
-
-  files.forEach(callback);
-
-  if (amountOfFiles > MAX_FILES_AMOUNT) {
-    console.log("too many files");
-  }
-
-  return amountOfFiles <= MAX_FILES_AMOUNT;
-};
-
-const validateFiles = (files) => {
-  const isFileSizesValid = validateSizesOfFiles(files);
-  const amountOfFiles = validateAmountOfFiles(files);
-  return isFileSizesValid && amountOfFiles;
-};
 
 const getUserByEmail = async (email) => {
   const query = gql`
