55c99e2e8b4e2df305d8160a6c47dd72b279f41f
Alex Barashkov <lnikell@gmail.com>
2021-05-25 18:33:45 +0200

    chore: add links to use cases

--- templates/frontend/src/components/shared/use-cases/use-cases.jsx
+++ templates/frontend/src/components/shared/use-cases/use-cases.jsx
@@ -35,21 +35,61 @@ UseCases.propTypes = {
 
 UseCases.defaultProps = {
   cases: [
-    { title: 'Kubernetes', url: '/' },
-    { title: 'Docker', url: '/' },
-    { title: 'React', url: '/' },
-    { title: 'Angular', url: '/' },
-    { title: 'Vue', url: '/' },
-    { title: 'Kubernetes', url: '/' },
-    { title: 'Docker', url: '/' },
-    { title: 'React', url: '/' },
-    { title: 'Angular', url: '/' },
-    { title: 'Vue', url: '/' },
-    { title: 'Kubernetes', url: '/' },
-    { title: 'Docker', url: '/' },
-    { title: 'React', url: '/' },
-    { title: 'Angular', url: '/' },
-    { title: 'Vue', url: '/' },
+    {
+      title: 'Kubernetes',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#kubernetes',
+    },
+    {
+      title: 'CI/CD',
+      url: 'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#cicd',
+    },
+    {
+      title: 'Docker',
+      url: 'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#docker',
+    },
+    {
+      title: 'React',
+      url: 'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#react',
+    },
+    {
+      title: 'Vue',
+      url: 'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#vue',
+    },
+    {
+      title: 'Graphql',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#graphql',
+    },
+    {
+      title: 'Webinars',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#webinars',
+    },
+    {
+      title: 'Starters',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#starters',
+    },
+    {
+      title: 'Node.js',
+      url: 'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#nodejs',
+    },
+    {
+      title: 'Tailwind',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#tailwind',
+    },
+    {
+      title: 'Wordpress',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#wordpress',
+    },
+    {
+      title: 'Linters',
+      url:
+        'https://github.com/snipsnapdev/snipsnap/blob/master/templates/docs/use-cases.md#linters',
+    },
   ],
 };
 
